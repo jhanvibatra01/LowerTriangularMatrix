f2b130e42bc9adbecdb139ae3008d2f713d647d1
jhanvibatra01 <43892827+jhanvibatra01@users.noreply.github.com>
2021-11-18 18:35:49 +0530

    Create LowerTriangularMatrix.java

---
+++ LowerTriangularMatrix.java
@@ -0,0 +1,39 @@
+package com.tw;
+
+import java.util.Scanner;
+
+public class LowerTriangularMatrix {
+    public static boolean checkLowerTriangularMatrix(int[][] matrix,int rows,int columns){
+
+        for(int i=0;i<rows;i++) {
+            for (int j = 0; j < columns; j++) {
+                if(j>i && matrix[i][j]!=0){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    
+    public static void main(String[] args){
+
+        Scanner sc=new Scanner(System.in);
+        int rows=sc.nextInt();
+        int columns=sc.nextInt();
+        int[][] matrix=new int[rows][columns];
+
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<columns;j++){
+                matrix[i][j]=sc.nextInt();
+            }
+        }
+
+        if(checkLowerTriangularMatrix(matrix,rows,columns))
+            System.out.println("yes");
+        
+        else
+            System.out.println("no");
+        
+
+    }
+}
